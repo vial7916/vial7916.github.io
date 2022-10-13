bbf9679b5549a6c660d7f2eb6ed1f4da4bb70acc
Victor Alamillo <vial7916@colorado.edu>
2022-10-12 21:37:12 -0600

    ica8

--- js/ica8.js
+++ js/ica8.js
@@ -4,3 +4,18 @@ body.addEventListener('click', giveAlert);
 function giveAlert() {
     alert('this is an alert');
 }
+
+const hover = document.getElementById("hover");
+
+hover.addEventListener("mouseover", (event) => {
+    event.target.style.color = "orange";
+    setTimeout(() => {
+        event.target.style.color = "";
+      }, 500);
+    }, false);
+
+
+function myFunction() {
+        var x = document.getElementById("fname");
+        x.value = x.value.toUpperCase();
+      }

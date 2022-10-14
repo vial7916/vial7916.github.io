d041451e4368a6241b208c7402035a3dafb339b7
Victor Alamillo <vial7916@colorado.edu>
2022-10-14 16:31:17 -0600

    assignment-8

--- Assignments/assignment-8/main.js
+++ Assignments/assignment-8/main.js
@@ -7,29 +7,36 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const storyText = 'It was 45 fahrenheit outside, so :insertx: decided to stay inside and play video games. An hour into the game, they got hungry so they decided to eat a :inserty:. The food tasted weird so they decided to look down and saw a :insertz: in the food. Victor saw the whole thing, but was not surprised — :insertx: had the worst of luck and something bad was always hapenning to them.';
+const insertX = ['Rocco', 'Kelea', 'Veena'];
+const insertY = ['bowl of fried rice', 'grilled cheese sandwich', 'cup of ramen'];
+const insertZ = ['rat', 'gnat', 'spider'];
 
 randomize.addEventListener('click', result);
 
 function result() {
-    
+    let newStory = storyText;
 
+const xItem = randomValueFromArray(insertX);
+const yItem = randomValueFromArray(insertY);
+const zItem = randomValueFromArray(insertZ);
 
+newStory = newStory.replaceAll(':insertx:',xItem);
+newStory = newStory.replaceAll(':inserty:',yItem);
+newStory = newStory.replaceAll(':insertz:',zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replaceAll('Victor', name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((45-32)* 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('45 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
